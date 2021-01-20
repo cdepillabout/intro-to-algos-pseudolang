c6d0528df48f112eb5b41f41090c8fda4c4e7b96
Dennis Gosnell <cdep.illabout@gmail.com>
2021-01-20 11:40:47 +0900

    Add command line arg parsing and add an actual example.

--- src/Pseudolang/Main.hs
+++ src/Pseudolang/Main.hs
@@ -4,5 +4,42 @@ module Pseudolang.Main
 
 import Pseudolang.Prelude
 
+import Options.Applicative (Parser, argument, execParser, fullDesc, header, help, helper, info, metavar, progDesc, str)
+
+import Pseudolang.Interpreter (parseAndInterpret)
+
+data CmdOpts = CmdOpts
+  { file :: FilePath
+  }
+
+cmdOptsParser :: Parser CmdOpts
+cmdOptsParser = do
+  let fileParser =
+        argument
+          str
+          ( metavar "FILE" <>
+            help "Pseudolang file to interpret"
+          )
+  CmdOpts <$> fileParser
+
+runOptionParser :: IO CmdOpts
+runOptionParser = execParser opts
+  where
+    opts :: _
+    opts =
+      info
+        (cmdOptsParser <**> helper)
+        ( fullDesc <>
+          progDesc "Interpret and run FILE" <>
+          header "pseudo - an interpreter for Pseudolang programs"
+        )
+
 defaultMain :: IO ()
-defaultMain = putStrLn "someFunc"
+defaultMain = do
+  cmdOpts <- runOptionParser
+  run cmdOpts
+
+run :: CmdOpts -> IO ()
+run (CmdOpts file) = do
+  fileContents <- readFile file
+  parseAndInterpret $ decodeUtf8 fileContents
--- test/Test/Pseudolang/InterpreterTest.hs
+++ test/Test/Pseudolang/InterpreterTest.hs
@@ -220,6 +220,7 @@ test =
               fun Insertion-Sort(A)
                 for j = 2 to A.length
                   key = A[j]
+                  // Insert A[j] into the sorted sequence A[1..j - 1]
                   i = j - 1
                   while i > 0 and A[i] > key
                     A[i + 1] = A[i]
