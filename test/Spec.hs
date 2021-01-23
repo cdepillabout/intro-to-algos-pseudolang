d5d8277437eeda608f83505e0d15d048f322f1db
Dennis Gosnell <cdep.illabout@gmail.com>
2021-01-23 11:07:40 +0900

    Add expected output for examples.

--- test/Spec.hs
+++ test/Spec.hs
@@ -5,12 +5,14 @@ import Pseudolang.Prelude
 
 import Test.Hspec (hspec)
 
+import qualified Test.Pseudolang.Golden as Golden
 import qualified Test.Pseudolang.InterpreterTest as InterpreterTest
 import qualified Test.Pseudolang.LexerTest as LexerTest
 import qualified Test.Pseudolang.ParserTest as ParserTest
 
 main :: IO ()
 main = hspec $ do
+  Golden.test
   LexerTest.test
   ParserTest.test
   InterpreterTest.test
